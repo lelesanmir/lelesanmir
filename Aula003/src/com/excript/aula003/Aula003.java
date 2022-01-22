f49dc5a077046c75f4efe35f4c046fc792c900d1
leona <leona@LAPTOP-5RIEOOUO>
2022-01-22 09:18:40 -0300

    Criação do projeto

---
+++ Aula003/src/com/excript/aula003/Aula003.java
@@ -0,0 +1,19 @@
+package com.excript.aula003;
+
+import java.util.Scanner;
+
+public class Aula003 {
+	
+	public static void main (String[] args) {
+	
+	int i = 0;
+	do {
+		System.out.println( "do/while" );
+	}while( 1 == 2);
+		System.out.println("while");
+	}	
+}
+	
+	
+	
+
