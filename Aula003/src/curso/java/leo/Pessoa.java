7fb4b08c6be63b11ff09a8a5129630045b076d1d
leona <leona@LAPTOP-5RIEOOUO>
2022-01-22 10:59:09 -0300

    adicionando o campo de idade na classe pessoa

--- Aula003/src/curso/java/leo/Pessoa.java
+++ Aula003/src/curso/java/leo/Pessoa.java
@@ -5,9 +5,19 @@ public class Pessoa {
 	private String nome;
 	
 	private String sobrenome;
+	
+	private int idade;
 
 	public String getNome() {
 		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 
 	public void setNome(String nome) {
